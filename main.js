aed5dbd38bcb8783c86c797eee928eba8ff889ac
Amit Arora <amitarora@Amits-MacBook-Air.local>
2026-09-15 20:37:03 +0530

    Rename main.html to index.html

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ document.getElementById('start-btn').addEventListener('click', () => {
     document.getElementById('start-btn').style.display = 'none';
     EndCredits.rollSequence();
 });
+<button id="start-btn">Initiate Final Sequence</button>;
+document.getElementById('start-btn').addEventListener('click', () => {
+    // Code happens here
+});
+document.getElementById('start-btn').style.display = 'none';
+EndCredits.rollSequence();
+setTimeout(() => {
+    UIManager.logEvent("All syndicate nodes disabled.");
+}, 2000);
