a5f3fe16623a0a9843957c7fef20cbf1aa3410d3
Amit Arora <amitarora@Amits-MacBook-Air.local>
2026-09-16 10:53:04 +0530

    Fix HTML and rename entry file

--- main.js
+++ main.js
@@ -13,3 +13,8 @@ EndCredits.rollSequence();
 setTimeout(() => {
     UIManager.logEvent("All syndicate nodes disabled.");
 }, 2000);
+document.getElementById('start-btn').addEventListener('click', () => {
+    // Code happens here
+});
+document.getElementById('start-btn').style.display = 'none';
+EndCredits.rollSequence();
